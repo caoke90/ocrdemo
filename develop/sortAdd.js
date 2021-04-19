909ab0e61dbb2a384a7bdcc4f07065b64b0a01bd
caoke90 <914890674@qq.com>
2021-04-19 21:14:49 +0800

    21

--- develop/sortAdd.js
+++ develop/sortAdd.js
@@ -1,54 +1,51 @@
-//比较两字符的相等长度和大小
-function compareLen(n1,n2,str1,str2) {
+//比较两字符的大小、相等长度、是否包含
+function compareLen(str1,str2,n1=0,n2=0) {
   //求出相等部分
-  var len=0;
-  while (n1+len<=str1.length&&n2+len<=str2.length&&str1.charCodeAt(n1+len)===str2.charCodeAt(n2+len)){
-    len++;
-  }
-  var code1=str1.charCodeAt(n1+len);
-  var code2=str2.charCodeAt(n2+len);
-
-  if(Number.isNaN(code1)&&Number.isNaN(code2)){
-    return [len,0]
-  }else if(Number.isNaN(code1)){
-    return [len,-1]
-  }else if(Number.isNaN(code2)){
-    return [len,1]
-  }else{
-    //求出大小
-    var dis=0;
-    if(code1<code2){
+  let len=0;
+  let dis=-2;
+  while (dis===-2){
+    const l1=n1+len
+    const l2=n2+len
+    if(l1<str1.length&&l2<str2.length&&str1.charCodeAt(l1)===str2.charCodeAt(l2)){
+      len++;
+    }else if(l1===str1.length&&l2===str2.length){
+      dis=0;
+    }else if(l1===str1.length){
       dis=-1
-    }else if(code1>code2){
+    }else if(l2===str2.length){
       dis=1
+    }else{
+      if(str1.charCodeAt(l1)>str2.charCodeAt(l2)){
+        dis=1
+      }else{
+        dis=-1
+      }
     }
-    if(str1.substr(n1,len)!==str2.substr(n2,len)){
-      console.log(dis,len,str1.substr(n1,len),'===',str2.substr(n2,len),n1,n2,str1.length,str2.length)
-    }
-
-    return [len,dis];
   }
+  return [dis,len]
 }
+
+
 //查找字符在数组的最大相等长度和大小
 function findLen(str,hasSortArr,callback) {
   var l=0,r=hasSortArr.length;
   var lock=-1;
   var len1=0,len2=0;
   var len=0,dis=0;
-
   if(hasSortArr.length>0){
-    [len1,dis]=callback(str,hasSortArr[0]);
+    [dis,len1]=callback(str,hasSortArr[0]);
     if(dis<1){
       return [0,len1,dis]
     }
-    [len2,dis]=callback(str,hasSortArr[r-1]);
+    [dis,len2]=callback(str,hasSortArr[r-1]);
     if(dis>-1){
       return [r-1,len2,dis]
     }
     while(lock===-1){
       var m=(l+r+1)>>1;
       //比较下坐标大小
-      [len,dis]=callback(str,hasSortArr[m])
+      [dis,len]=callback(str,hasSortArr[m])
+
       if(dis===1){
         if(m+1===r){
           if(len<len2){
@@ -87,13 +84,13 @@ function findLen(str,hasSortArr,callback) {
 //[n,len,dis]
 Array.prototype.sortFindLen=function(key,n1=0,n2=0){
   return findLen(key,this,function (str1,obj) {
-    return compareLen(n1,n2,str1,obj.key)
+    return compareLen(str1,obj.key,n1,n2)
   })
 }
 //添加字符到排序数组中
 function sortAdd(key,val,dataMap){
   if(typeof key==='string'){
-    const [n,len,dis]=tzArr.sortFindLen(key)
+    const [n,len,dis]=dataMap.sortFindLen(key)
     if(dis===1){
       dataMap.splice(n+1,0,{key,data:[val]})
     }else if(dis===-1){
@@ -104,9 +101,4 @@ function sortAdd(key,val,dataMap){
   }
 }
 module.exports=sortAdd
-// const arr=['11','21']
-// arr.sortAdd('1')
-// arr.sortAdd('111')
-// arr.sortAdd('0')
-// arr.sortAdd('3')
-// console.log(arr)
+
