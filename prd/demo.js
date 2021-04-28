b1181804605a98a4c3a4c59ec4bbfe84944a828a
caoke <914890674@qq.com>
2021-04-28 18:50:13 +0800

    21

--- prd/demo.js
+++ prd/demo.js
@@ -5,7 +5,9 @@ const getV=require('../develop/getV')
 const getLineArrByGrayData=require('../develop/getLineArrByGrayData')
 const getStrByNum=require('../develop/getStrByNum')
 const {sortAdd,sortFind}=require('../develop/sortAdd')
+const {getTz,getTzY,getTzArr}=require('../develop/tzCommon')
 const {renderTextInit,renderTextToImageData}=require('../develop/renderTextToImageData')
+const mekflink=require('./mekflink')
 renderTextInit()
 function changeV(x,y,imageData) {
   let k=4*imageData.width*y+4*x;
@@ -51,69 +53,16 @@ function setOpacity(grayData,imageData) {
     }
   }
 }
-
-
-function getSameH(x,y,grayData) {
-  let h=1;
-  while (getV(x,y+h,grayData)!==undefined){
-    h++;
+function showRect(pos1,imageData) {
+  const [x1,y1,x2,y2]=pos1;
+  for(let i=x1;i<=x2;i++){
+    changeV1(i,y1,imageData)
+    changeV1(i,y2,imageData)
   }
-  return h;
-}
-function getSameW(x,y,grayData) {
-  let h=1;
-  while (getV(x+h,y,grayData)!==undefined){
-    h++;
+  for(let i=y1;i<=y2;i++){
+    changeV1(x1,i,imageData)
+    changeV1(x2,i,imageData)
   }
-  return h;
-}
-function getTzArr (posArr,grayData) {
-  const tzArr=[]
-  posArr.forEach(function (pos1,i) {
-    const tz=getTz(pos1,grayData)
-    tzArr.push(tz)
-  })
-  return tzArr;
-}
-function getTz (pos1,grayData) {
-  const [x1, y1, x2, y2] = pos1
-  const fArr = [];
-
-  for (let x = x1; x < x2; x++) {
-    let hu = 0;
-    for (let y = y1; y < y2; y++) {
-      const v = getV(x, y, grayData)
-      if (v !== undefined) {
-        hu++;
-      }
-    }
-    if(hu===0&&fArr.length===0){
-
-    }else{
-      fArr.push(getStrByNum(hu));
-    }
-
-  }
-  return fArr.join('');
-}
-function getTzY (pos1,grayData) {
-  const [x1, y1, x2, y2] = pos1
-  const fArr = [];
-  for (let y = y1; y < y2; y++) {
-    let hu = 0;
-    for (let x = x1; x < x2; x++) {
-      const v = getV(x, y, grayData)
-      if (v !== undefined) {
-        hu++;
-      }
-    }
-    if(hu===0&&fArr.length===0){
-
-    }else{
-      fArr.push(getStrByNum(hu));
-    }
-  }
-  return fArr.join('');
 }
 const prdMap=JSON.parse(fs.readFileSync('prdMap.json').toString())
 
@@ -121,9 +70,19 @@ function saveImageToFile(imageData,filename) {
   var buffer = PNG.sync.write(imageData, {filterType: 4});
   fs.writeFileSync(filename,buffer)
 }
-
+prdMap.forEach(function ([key,val],i) {
+  if(!Array.isArray(val)){
+    mekflink.add(key,val)
+  }else{
+    val.forEach(function (arr) {
+      mekflink.add(key,arr[1])
+    })
+  }
+})
+mekflink.makeGl()
 function demo () {
-  const buff=fs.readFileSync('../data/Arial31.png');
+  console.time('demo')
+  const buff=fs.readFileSync('../data/test.png');
   const imageData=PNG.sync.read(buff)
   const grayData=getGrayData(imageData)
   const posArr=getLineArrByGrayData(grayData)
@@ -132,74 +91,75 @@ function demo () {
 
   const tzArr=getTzArr(posArr,grayData)
   tzArr.forEach(function (tz,m) {
+    const arr=mekflink.fastSolve(tz)
+    console.log(arr)
     const pos1=posArr[m]
+    showRect(pos1,imageData)
     const tarr=[]
+    const tzarr=[]
     let i=0;
-    let d='';
+    let dArr=[];
     let preText='';
     while (i<tz.length){
-      if(d===''&&tz[i]==='0'){
-        i++
-      }else{
-        const arr=sortFind(tz,i,prdMap)
-        let isOk=true;
-        if(arr&&arr[2]===0){
-          const data=prdMap[arr[0]]
-          if(Array.isArray(data[1])&&Array.isArray(data[1][0])){
-            const key2=getTzY([pos1[0]+i,pos1[1],pos1[0]+i+arr[1],pos1[3]],grayData)
-            const [n,len,dis]=data[1].sortFindLen(key2)
-            if(dis===0){
-              tarr.push(data[1][n][1])
-            }else{
-              //找不到相似的
-              isOk=false
-            }
-          }else if(Array.isArray(data[1])){
-            //找出最近的
-            let minNum;
-            let minI;
-            const preCode=preText.charCodeAt(0);
-            for(let h=0;h<data[1].length;h++){
-              const code=data[1][h].charCodeAt(0)
-              const num=Math.abs(preCode-code);
-              if(h===0){
-                minNum=num;
-                minI=h;
-              }else if(num<minNum){
-                minNum=num;
-                minI=h;
-              }
-            }
-            preText=data[1][minI]
-            tarr.push(data[1][minI])
-          }else{
-            preText=data[1]
-            tarr.push(data[1])
+
+      const arr=sortFind(tz,i,prdMap)
+      let text='';
+      if(arr&&arr[2]===0){
+        const data=prdMap[arr[0]]
+        if(Array.isArray(data[1])&&Array.isArray(data[1][0])){
+          const key2=getTzY([pos1[0]+i,pos1[1],pos1[0]+i+arr[1],pos1[3]],grayData)
+          const [n,len,dis]=data[1].sortFindLen(key2)
+          if(dis===0){
+            tarr.push(data[1][n][1])
           }
-        }
-        if(isOk){
-          if(d){
-            tarr.push(d);
-            d='';
+        }else if(Array.isArray(data[1])){
+          //找出最近的
+          let minNum;
+          let minI;
+          const preCode=preText.charCodeAt(0);
+          for(let h=0;h<data[1].length;h++){
+            const code=data[1][h].charCodeAt(0)
+            const num=Math.abs(preCode-code);
+            if(h===0){
+              minNum=num;
+              minI=h;
+            }else if(num<minNum){
+              minNum=num;
+              minI=h;
+            }
           }
-          i=i+arr[1];
+          preText=data[1][minI]
+          text=data[1][minI]
         }else{
-          d=d+tz[i];
-          if(i===tz.length.length-1&&d){
-            tarr.push(d);
-            d='';
-          }
-          i++
+          preText=data[1]
+          text=data[1]
         }
       }
+      if(text){
+        if(dArr.length>0){
+          tarr.push('');
+          tzarr.push(dArr.join(''));
+          dArr=[];
+        }
+        tarr.push(text)
+        tzarr.push(tz.substr(i,arr[1]))
+        i=i+arr[1];
+      }else{
+        dArr.push(tz[i]);
+        if(i===tz.length-1&&dArr.length>0){
+          tarr.push('');
+          tzarr.push(dArr.join(''))
+          dArr=[]
+        }
+        i++
+      }
     }
-    renderTextToImageData(tarr,pos1,imageData)
-
-    if(tarr.length>1){
-      console.log('tarr',tarr)
+    if(tarr.length>0){
+      console.log(tarr,tzarr)
+      // renderTextToImageData(tarr.join(''),pos1,imageData)
     }
   })
-
+  console.timeEnd('demo')
   saveImageToFile(imageData,'demo2.png')
 }
 
